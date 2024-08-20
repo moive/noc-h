88e3efc38fa5f8cb08ab567bd5b5b59a78324686
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-08-19 23:20:26 -0500

    types or undefined

--- src/domain/use-cases/checks/check.service.ts
+++ src/domain/use-cases/checks/check.service.ts
@@ -5,8 +5,8 @@ interface ICheckServiceUseCase {
 	execute(url: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements ICheckServiceUseCase {
 	constructor(
@@ -25,13 +25,13 @@ export class CheckService implements ICheckServiceUseCase {
 				LogSeverityLevel.low
 			);
 			this.logRepository.saveLog(log);
-			this.successCallback();
+			this.successCallback && this.successCallback();
 			return true;
 		} catch (err) {
 			const errorMessage = `${url} is not ok. ${err}`;
 			const log = new LogEntity(errorMessage, LogSeverityLevel.high);
 			this.logRepository.saveLog(log);
-			this.errorCallback(errorMessage);
+			this.errorCallback && this.errorCallback(errorMessage);
 
 			return false;
 		}
