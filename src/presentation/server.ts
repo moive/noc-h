b1e96a21cba3884e86be6b04fccf8b466e9a326e
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-08-22 08:57:06 -0500

    work nodemailer with gmail

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from "../domain/use-cases/checks/check.service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImplementation } from "../infrastructure/repositories/log.repository.implementation";
 import { CronService } from "./cron/cron.service";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImplementation(
 	new FileSystemDatasource()
@@ -10,16 +11,29 @@ const fileSystemLogRepository = new LogRepositoryImplementation(
 export class Server {
 	public static start() {
 		console.log("Server started...");
-		CronService.createJon("*/5 * * * * *", () => {
-			// const date = new Date();
-			// console.log("5 second", date);
-			// const url = "https://localhost:3000";
-			const url = "https://google.com";
-			new CheckService(
-				fileSystemLogRepository,
-				() => console.log(`${url} is ok`),
-				(error) => console.log(error)
-			).execute(url);
+
+		// Send email
+		const emailService = new EmailService();
+		emailService.sendMail({
+			to: "mvelasquezdeveloper@gmail.com",
+			subject: "Logs System",
+			htmlBody: `
+			<h2>Logs System</h2>
+			<p>This is a email test 🚩</p>
+			<p>See logs attach</p>
+			`,
 		});
+
+		// CronService.createJon("*/5 * * * * *", () => {
+		// 	// const date = new Date();
+		// 	// console.log("5 second", date);
+		// 	// const url = "https://localhost:3000";
+		// 	const url = "https://google.com";
+		// 	new CheckService(
+		// 		fileSystemLogRepository,
+		// 		() => console.log(`${url} is ok`),
+		// 		(error) => console.log(error)
+		// 	).execute(url);
+		// });
 	}
 }
