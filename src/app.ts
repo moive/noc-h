192aaa119d72d64fe2ba2689a998ddb20a476592
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-10-31 17:38:31 -0500

    MongoLogDatasource

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { MongoDatabase } from "./data/mongo";
+import { LogModel, MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
 import { envs } from "./config/plugins/envs.plugin";
 
@@ -12,6 +12,17 @@ async function main() {
 		mongoUrl: envs.MONGO_URL,
 		dbName: envs.MONGO_DB_NAME,
 	});
+
+	/* const newLog = await LogModel.create({
+		message: "Test message from MongoDB",
+		origin: "App.ts",
+		level: "low",
+	});
+
+	await newLog.save(); */
+
+	const logs = await LogModel.find({}, { _id: 0, message: 1 });
+	console.log(logs);
 
 	// Server.start();
 	// console.log(envs);
